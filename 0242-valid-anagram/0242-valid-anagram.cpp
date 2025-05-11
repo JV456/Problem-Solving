3fc18bf3a4600954a97fc0a9d7f91c564f541b7f
Jai Vadula <99525324+JV456@users.noreply.github.com>
2025-05-12 00:23:23 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        vector<int> count(26, 0);
 
-        return s == t;
+        for(char &ch : s){
+            count[ch-'a']++;
+        }
+
+        for(char &ch : t){
+            count[ch-'a']--;
+        }
+
+        bool allZeros = all_of(begin(count), end(count), [](int element){
+            return element == 0;
+        });
+
+        return allZeros;
     }
 };
