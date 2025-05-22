f681fb91a9fb946fe4a303b028c41ef66d1eebd8
Jai Vadula <99525324+JV456@users.noreply.github.com>
2025-05-22 21:14:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int firstRepeated(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        std::unordered_map<int, int> freq;
+        for (int i = 0; i < n; ++i) {
+            freq[arr[i]]++;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (freq[arr[i]] > 1) return i + 1;
+        }
+        return -1;
+    }
+};
