ea08413aacc346b3ec7d9ac6afa238a303ef2923
Jai Vadula <99525324+JV456@users.noreply.github.com>
2025-06-21 19:33:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non-Repeating Element/non-repeating-element.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+  public:
+    int firstNonRepeating(vector<int>& arr) {
+        // Complete the function
+        unordered_map<int, int> freq;
+        for (int num : arr) freq[num]++;
+        for (int num : arr) if (freq[num] == 1) return num;
+        return 0;
+    }
+};
